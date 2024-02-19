d455f0c9ac5b10e742728931c968621b78dd01bf
Kally95 <kally-95@hotmail.com>
2024-02-19 23:44:05 +0000

    initial!

---
+++ MazeSolve.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(width, height):
+        self.width = width
+        self.height = height
+        widget = Tk()
+        self.title = widget
+        self.canvas = 
+    
