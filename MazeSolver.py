0d65a9911ce26b9dac11b469e5d0ecf7b0750ee0
Kally95 <kally-95@hotmail.com>
2024-02-20 13:17:35 +0000

    Can now draw lines on canvas

--- MazeSolver.py
+++ MazeSolver.py
@@ -1,11 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
-
+from Point import Line, Point
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("My Window")
         self.canvas = Canvas(self.root, width=width, height=height)
-        self.__canvas.pack(fill=BOTH, expand=1)
+        self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -21,10 +21,17 @@ class Window:
     def close(self):
         self.running = False
         self.root.destroy()
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
     
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    p1 = Point(1, 1)
+    p2 = Point(100, 100)
+    line = Line(p1, p2)
+    win.draw_line(line, "black")
+    win.wait_for_close() 
 
 if __name__ == "__main__":
     main()
