57692419d0ee9be49204b551ef1ae69d7f30c15f
Kally95 <kally-95@hotmail.com>
2024-02-20 00:28:24 +0000

    Canvas now gets created

--- MazeSolve.py
+++ MazeSolve.py
@@ -1,10 +1,30 @@
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(width, height):
-        self.width = width
-        self.height = height
-        widget = Tk()
-        self.title = widget
-        self.canvas = 
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("My Window")
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
     
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+        
+    def close(self):
+        self.running = False
+        self.root.destroy()
+    
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
