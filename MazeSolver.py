c04a8fcc5e21f015616f6bba379d3ae2e0917bf6
Kally95 <kally-95@hotmail.com>
2024-02-20 18:51:08 +0000

    Added cell class and updated main to draw cells

--- MazeSolver.py
+++ MazeSolver.py
@@ -1,37 +1,54 @@
 from tkinter import Tk, BOTH, Canvas
 from Point import Line, Point
+from Cell import Cell
+
 class Window:
     def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("My Window")
-        self.canvas = Canvas(self.root, width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-    
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-        
-    def close(self):
-        self.running = False
-        self.root.destroy()
+        self.__root.update_idletasks()
+        self.__root.update()
 
-    def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
-    
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+    def close(self):
+        self.__running = False
+
+
 def main():
     win = Window(800, 600)
-    p1 = Point(1, 1)
-    p2 = Point(100, 100)
-    line = Line(p1, p2)
-    win.draw_line(line, "black")
-    win.wait_for_close() 
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
+    win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
